f386ba97a66c51b8fad83a116c170acfaf4ba249
Yohan Hartanto <yohan@uber.com>
2022-05-03 11:17:14 -0700

    moved p10 context to left

--- zsh/.p10k.zsh
+++ zsh/.p10k.zsh
@@ -34,6 +34,7 @@
     os_icon                 # os identifier
     dir                     # current directory
     vcs                     # git status
+    context
     # prompt_char           # prompt symbol
   )
 
@@ -81,7 +82,6 @@
     gcloud                  # google cloud cli account and project (https://cloud.google.com/)
     google_app_cred         # google application credentials (https://cloud.google.com/docs/authentication/production)
     toolbox                 # toolbox name (https://github.com/containers/toolbox)
-    context                 # user@hostname
     nordvpn                 # nordvpn connection status, linux only (https://nordvpn.com/)
     ranger                  # ranger shell (https://github.com/ranger/ranger)
     nnn                     # nnn shell (https://github.com/jarun/nnn)
